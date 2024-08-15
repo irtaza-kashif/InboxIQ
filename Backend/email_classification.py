a72e6ff82f8930caa03433b5a29417af5d4b03ec
Irtaza Kashif <irtaza.kashif@gmail.com>
2024-08-15 16:59:59 -0700

    Revert "ADDED FUNCTIONS:"
    This reverts commit 40ac2dce62cee8cf56992699efbbfbdad84b7163.

--- Backend/email_classification.py
+++ Backend/email_classification.py
@@ -1,4 +1,3 @@
-# IMPORTS
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
@@ -6,94 +5,79 @@ import os
 import base64
 import html
 import string
+
 import re
+creds = None
+if os.path.exists('token.json'):
+    creds = Credentials.from_authorized_user_file('token.json', ['https://www.googleapis.com/auth/gmail.readonly'])
+else:
+    flow = InstalledAppFlow.from_client_secrets_file('credentials.json', ['https://www.googleapis.com/auth/gmail.readonly'])
+    creds = flow.run_local_server(port=0)
 
-# Gathering credentials and tokens, authorizing email and API access
-def authenticate():
-    creds = None
-    # Note: file path for token.json and credentials.json subject to change, not sure how to standardize a path for this yet
-    # Feel free to change it to whatever works for you when you're running this file and recommit it to the Git Repo, I won't mind
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file(r'Backend\token.json', ['https://www.googleapis.com/auth/gmail.readonly'])
-    else:
-        flow = InstalledAppFlow.from_client_secrets_file(r'Backend\credentials.json', ['https://www.googleapis.com/auth/gmail.readonly'])
-        creds = flow.run_local_server(port=0)
+with open('token.json', 'w') as token:
+    token.write(creds.to_json())
 
-    with open('token.json', 'w') as token:
-        token.write(creds.to_json())
+service = build('gmail', 'v1', credentials=creds)
 
-    global service
-    service = build('gmail', 'v1', credentials=creds)
-
-# Gathers all new emails not yet checked by InboxIQ and returns them in a list of dictionaries where each dictionary contains the un=parsed data of one email
-def retrieveUncheckedEmailContents():
-    results = service.users().messages().list(maxResults=500, q="has:nouserlabels", userId="me").execute()
-    unreadEmails = results.get('messages', [])
-    ids = retrieveEmailIds(unreadEmails)
-    return retrieveEmailContents(ids)
-# Given a list of email IDs, will return un-parsed message data for all of them in a list of dictionaries
-def retrieveEmailContents(ids):
-    messages = []
-    for id in ids:
-        messages.append(service.users().messages().get(userId='me', id=id).execute())
-    return messages
-# Given a list of dictionaries containing email IDs and other data, will return a list of just the email IDs
-def retrieveEmailIds(unreadEmails):
-    ids = []
-    for email in unreadEmails:
-        ids.append(email["id"])
-    return ids
-# Parses email data and stores each email in its own dictionary with key-value pairs for the sender, reciever, subject line, message body, etc.
-# Returns a dictionary containing key-value pairs of emailID:dictionary of email data for the corresponding email
-def parseEmailData(emails):
-    # Create outer dictionary that will be returned
-    parsedEmailData = {}
-    # Iterate over each email
-    for email in emails:
-        # Create temporary dictionary to hold data for the current email
-        tempEmailDict = {}
-        emailPayload = email["payload"]
-        # Retrieve all header data for the current email
-        dictKeys = emailPayload["headers"]
-        # Search for desired header data in list of all header data from the current email
-        for key in dictKeys:
-            if key["name"] == 'From':
-                # Note: the .get() command allows us to specify a default value to use in the case that the key we're looking for (in this case "value") does not exist
-                # This negates the need for try catch statements as a KeyError exception is impossible (i hope)
-                tempEmailDict["msgSender"] = key.get("value", "No Sender Specified")
-            if key["name"]  == 'To':
-                tempEmailDict["msgReciever"] = key.get("value", "No Reciever Specified")
-            if key["name"] == 'Subject':
-                tempEmailDict["msgSubject"] = key.get("value", "No Subject Specified")
-                # Subject line and message body struggle the most with returning blank strings and failing to label them as "No Subject Specified" or "No Email Body"
-                # This fixes that annoying issue
-                if tempEmailDict["msgSubject"] == "":
-                        tempEmailDict["msgSubject"] = "No Subject Specified"
-            if key["name"] == 'Date':
-                tempEmailDict["msgDate"] = key.get("value", "No Date Specified")
-        
-        # Setting default value for the "msgBody" key in the case that we can't find a message body in the email data
-        tempEmailDict.setdefault("msgBody", "No Email Body")
-        # Emails with attachments vs without attachments require us to go down different paths to retrieve the message body data
-        # If email has no attachment...
-        if "attachmentId" not in emailPayload["parts"][1]["body"]:
-            if "data" in emailPayload["parts"][0]["body"]:
-                tempEmailDict["msgBody"] = str(base64.urlsafe_b64decode(emailPayload["parts"][0]["body"]["data"]+"==").decode('utf-8'))
-                tempEmailDict["msgBody"] = tempEmailDict["msgBody"].replace("\r", "").replace("\n", "")
-        # If email has yes attachment...
+def retrieve_unchecked_emails():
+    results = service.users().messages().list(maxResults=500, userId='cleverinboxinc@gmail.com').execute()
+    unread_emails = results.get('messages', [])
+    retrieved_emails = []
+    for email in unread_emails:
+        retrieved_emails.append(retrieve_message(email["id"]))
+    return retrieved_emails
+def retrieve_message(id):
+    message = service.users().messages().get(userId='cleverinboxinc@gmail.com', id=id).execute()
+    return message
+# HOW TO ACCESS ATTACAHMENTS: print(emails[0]["parts"][1]["body"]["attachmentId"])    
+emails = retrieve_unchecked_emails()
+for email in emails:
+    email_payload = email["payload"]
+    email_headers = email_payload["headers"]
+    msg_sender = "No Sender Specified"
+    msg_reciever = "No Reciever Specified"
+    msg_date = "No Date Specified"
+    msg_subject = "No Subject Specified"
+    for header in email_headers:
+        if header["name"] == 'From':
+            try:
+                msg_sender = header['value']
+            except KeyError:
+                msg_sender = "No Sender Specified"
+        if header["name"]  == 'To':
+            try:
+                msg_reciever = header['value']
+            except KeyError:
+                msg_reciever = "No Reciever Specified"
+        if header["name"] == 'Subject':
+            try:
+                msg_subject = header['value']
+            except KeyError:
+                msg_subject = "No Subject Specified"
+        if header["name"] == 'Date':
+            try:
+                msg_date = header['value']
+            except KeyError:
+                msg_date = "No Date Specified"
+    
+    # Pulls the body of the email, either as plain text or as an attachment. If it's an attachment, it decodes the base64 data and prints it.
+    if "attachmentId" not in email_payload["parts"][1]["body"]:
+        if "data" in email_payload["parts"][0]["body"]:
+            msg_body = str(base64.urlsafe_b64decode(email_payload["parts"][0]["body"]["data"]+"==").decode('utf-8')).replace(r"\r", "").replace(r"\n", "\n")
         else:
-            if "data" in emailPayload["parts"][0]["parts"][0]["body"]:
-                tempEmailDict["msgBody"] = str(base64.urlsafe_b64decode(emailPayload["parts"][0]["parts"][0]["body"]["data"] + "==").decode('utf-8'))
-                tempEmailDict["msgBody"] = tempEmailDict["msgBody"].replace("\r", "").replace("\n", "")
-        # Same thing as the msgSubject conditional, Gmail API fails to label a blank space as "No Email Body" so we step in and fix that
-        if tempEmailDict["msgBody"] == "":
-            tempEmailDict["msgBody"] = "No Email Body"
-        # Add our newly constructed dictioanry of data for the current email into the outer dictioanry with the key being the email ID
-        parsedEmailData[str(email["id"])] = tempEmailDict
-    return parsedEmailData
-
-#                                    \/ This is the index of the email in the list of emails returned by retrieceUncheckedEmailContents()
-# HOW TO ACCESS ATTACAHMENTS: emails[0]["payload"]["parts"][1]["body"]["attachmentId"]
-authenticate()
-emails = retrieveUncheckedEmailContents()
-data = parseEmailData(emails)
+            email_payload["parts"][0]["body"].setdefault("data", base64.urlsafe_b64encode(bytes("No Email Body", "utf-8")))
+            msg_body = base64.urlsafe_b64decode(email_payload["parts"][0]["body"]["data"] + b"==").decode('utf-8')
+    
+    else:
+        if "data" in email_payload["parts"][0]["parts"][0]["body"]:
+            msg_body = str(base64.urlsafe_b64decode(email_payload["parts"][0]["parts"][0]["body"]["data"] + "==").decode('utf-8')).replace(r"\r", "").replace(r"\n", "\n")
+        else:
+            email_payload["parts"][0]["parts"][0]["body"].setdefault("data", base64.urlsafe_b64encode(bytes("No Email Body", "utf-8")))
+            msg_body =base64.urlsafe_b64decode(email_payload["parts"][0]["parts"][0]["body"]["data"] + b"==").decode('utf-8')
+    
+    print(f"From: {msg_sender}")   
+    print(f"To: {msg_reciever}")
+    print(f"Date: {msg_date}")
+    print(f"Subject: {msg_subject}")
+    print(f"Body: {msg_body}\n")
+    print("\n\n\n")
